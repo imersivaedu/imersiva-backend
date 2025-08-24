afa9a168774dec8dfbd505a5531fe20c37600371
Rodrigo Lages <rodrigo_lagesbarbosa@hotmail.com>
2025-08-24 14:15:35 -0300

    update package dependencies and configure CORS in the app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,31 +1,50 @@
 import express from "express";
+import cors from "cors";
 import { setupWebSocket } from "../infra/websocket/serverSocket";
-import  http from 'http';
-import { cityRouter, classRouter, schoolRouter, studentRouter, userRouter, authRouter, experienceRouter } from "./routers";
-import { asyncErrors } from './middlewares/asyncErrors'
+import http from "http";
+import {
+  cityRouter,
+  classRouter,
+  schoolRouter,
+  studentRouter,
+  userRouter,
+  authRouter,
+  experienceRouter,
+} from "./routers";
+import { asyncErrors } from "./middlewares/asyncErrors";
 
 const app = express();
 const server = http.createServer(app);
+
+// Configure CORS to allow requests from frontend
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 setupWebSocket(server);
 
 app.get("/", (req, res) => {
   res.send("working working!!!!");
-})
-app.use('/student', studentRouter)
-app.use('/school', schoolRouter)
-app.use('/user', userRouter)
-app.use('/auth', authRouter)
-app.use('/class', classRouter)
-app.use('/city', cityRouter)
-app.use('/auth', authRouter)
-app.use('/experience', experienceRouter)
-import swaggerUi from 'swagger-ui-express';
-const swaggerDocument = require('../infra/docs/swagger-api.json');
+});
+app.use("/student", studentRouter);
+app.use("/school", schoolRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/class", classRouter);
+app.use("/city", cityRouter);
+app.use("/auth", authRouter);
+app.use("/experience", experienceRouter);
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = require("../infra/docs/swagger-api.json");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(asyncErrors as any)
+app.use(asyncErrors as any);
 
-export {app}
+export { app };
